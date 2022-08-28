cc6b87d06c78099ea784314bbb82d5eb163e5b4c
vsahasrabudhe96 <vsahasrabudhe96@gmail.com>
2022-08-28 17:21:16 -0400

    Data folder creation and incorporation in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from turtle import color
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,13 +6,18 @@ import time
 import mediapipe as mp
 import cv2
 from src.keypoint import draw_holistics,keypoint_detection,draw_styled_landmarks
-
+from src.datafolder import create_data_collection_folders
 
 
 
 
 
 if __name__ == "__main__":
+    DATA_PATH = "data/"
+    actions = np.array(['hello','thank you','I love you']) ## Actions we try to detect
+    no_sequences = 30  ## no_sequences == number of videos we want to collect e.g here we will collect 30 videos
+    sequence_length = 30 ## Each video will be of 30 frames
+    create_data_collection_folders(DATA_PATH,actions,no_sequences)
     mp_holistic,mp_drawing = draw_holistics()
     cap = cv2.VideoCapture(0)
     with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
