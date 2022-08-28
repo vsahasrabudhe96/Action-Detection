f866493ffc573e9b02b5eb58980855aa9b788d4d
vsahasrabudhe96 <vsahasrabudhe96@gmail.com>
2022-08-27 20:23:50 -0400

    Code for Video Capture

--- SignLanguageDetection.py
+++ SignLanguageDetection.py
@@ -1,6 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import time
+import mediapipe as mp
 import cv2
 
-
+## Open the camera to capture
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
